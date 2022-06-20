03be31295e53378f4e3fd27f5c290f32a9c7e145
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-06-20 14:00:49 -0400

    completed server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(require('./routes'));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/networkapi', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
